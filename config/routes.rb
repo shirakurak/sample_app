0bbf40eecfc721d289f153416a3f9b9ac2d1474a
shirakurak <ken1shirakura@gmail.com>
2022-09-22 09:33:53 +0900

    usersモデル生成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
