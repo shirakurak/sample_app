90295e6b2969d511d8cebc798aa1bd0510173971
shirakurak <ken1shirakura@gmail.com>
2022-09-27 11:37:23 +0900

    ~10.2

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ gem "bootsnap", require: false
 
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'faker', '~> 1.4', '>= 1.4.2'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @user = User.all
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -24,7 +30,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
+    if @user.update_attribute(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
     else
@@ -33,8 +39,18 @@ class UsersController < ApplicationController
   end
 
   private
-
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
