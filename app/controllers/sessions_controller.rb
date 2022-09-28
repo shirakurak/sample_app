ddd72b2e5cbd4c3a2711fb4b8bf657e3d176edfc
shirakurak <ken1shirakura@gmail.com>
2022-09-28 10:26:05 +0900

    ~色々壊れていたところを直した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,9 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or_to user
+      # redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @user = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
   def show
     @user = User.find(params[:id])
