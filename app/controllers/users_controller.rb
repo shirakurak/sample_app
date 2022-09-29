677083b4bb8c266667805684c25377de8470416e
shirakurak <ken1shirakura@gmail.com>
2022-09-29 10:50:04 +0900

    13.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,14 +53,6 @@ class UsersController < ApplicationController
 
   private
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
-  end
-
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
